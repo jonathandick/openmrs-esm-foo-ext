f9a7ae789eac119e95ab78426e74cf26885b8dbd
Jonathan Dick <jdick@ampath.or.ke>
2020-12-03 16:13:55 -0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import "./set-public-path";
 import { backendDependencies } from "./openmrs-backend-dependencies";
 import { attach } from "@openmrs/esm-extensions";
-import { defineConfigSchema, useExtensionConfig } from "@openmrs/esm-config";
+import { defineConfigSchema } from "@openmrs/esm-config";
 import { esmFooExtSchema } from "./config-schemas/openmrs-esm-foo-ext-schema";
+import {getAsyncLifecycle} from "@openmrs/esm-react-utils";
 
 const importTranslation = require.context(
   "../translations",
@@ -12,11 +13,19 @@ const importTranslation = require.context(
 );
 
 function setupOpenMRS() {
+  const moduleName = "@openmrs/esm-foo-ext-app";
+
+  const options = {
+    featureName: "foo",
+    moduleName
+  };
+
   return {
     extensions: [
       {
-        name: "foo-ext",
-        load: () => import("./openmrs-esm-foo-ext"),
+        id: "foo-ext",
+        slot: 'nav-menu',
+        load: getAsyncLifecycle(() =>  import("./openmrs-esm-foo-ext"), options),
       }
     ]
   };
